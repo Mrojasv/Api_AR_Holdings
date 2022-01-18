b767f1391a677e1c39ae66c3c49b5dc5fd4922fd
Mauricio Rojas Vindas <billincr@gmail.com>
2022-01-18 15:06:07 -0600

    Products

---
+++ AR_Holdings/Models/Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AR_Holdings.Models
+{
+    public class Products
+    {
+        [Table("ColaArticulos")]
+        public class ColaArticulos
+        {
+            [Key]
+            public int ID { get; set; }
+            [Required]
+            public string SKU { get; set; }
+            [Required]
+            public int Cantidad { get; set; }
+            [Required]
+            public DateTime FechaRegistro { get; set; }
+            public DateTime FechaActualizacion { get; set; }
+            public bool Sincronizado { get; set; }
+        }
+
+        public class ShopifyInventory
+        {
+            public string sku { get; set; }
+            public int available { get; set; }
+        }
+    }
+}
--- AR_Holdings/Services/Synchronize.cs
+++ AR_Holdings/Services/Synchronize.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using AR_Holdings.DBContent;
+using Dapper;
+using static AR_Holdings.Models.Products;
+
 namespace AR_Holdings.Services
 {
     public interface ISynchronize
@@ -8,9 +15,74 @@ namespace AR_Holdings.Services
 
     public class Synchronize: ISynchronize
     {
+        private readonly IDapper _Dapper;
+
+        public Synchronize(IDapper Dapper)
+        {
+            _Dapper = Dapper;
+        }
+
+
         public void ProductsShopify()
         {
+            using IDbConnection db = _Dapper.GetDbconnection();
+            try
+            {
+                DateTime _date = DateTime.Now;
+                var dbparams = new DynamicParameters();
 
+                if (db.State == ConnectionState.Closed)
+                    db.Open();
+
+                using var tran = db.BeginTransaction();
+                try
+                {
+                    var _ColaArticulos = db.Query<ColaArticulos>($"SELECT ID, SKU, Cantidad, Sincronizado " +
+                                        $"FROM ColaArticulos ", null, commandType: CommandType.Text, transaction: tran).ToList();
+
+                    //Obtener lista del inventario de Shopify, requiero accesToken??
+                    var _ShopifyInventory = new List<ShopifyInventory>();
+
+                    foreach (ColaArticulos _product in _ColaArticulos)
+                    {
+                        _product.Sincronizado = false;
+                        var _shopifyProduct = _ShopifyInventory.Where(w => w.sku == _product.SKU).FirstOrDefault();
+                        if(!string.IsNullOrEmpty(_shopifyProduct.sku))
+                        {
+                            _product.Cantidad = _shopifyProduct.available;
+                            _product.Sincronizado = true;
+                        }
+
+                        dbparams = new DynamicParameters();
+                        dbparams.Add("ID", _product.ID, DbType.Int32);
+                        dbparams.Add("Cantidad", _product.Cantidad, DbType.Int32);
+                        dbparams.Add("FechaActualizacion", _date, DbType.DateTime);
+                        dbparams.Add("Sincronizado", _product.Sincronizado, DbType.Boolean);
+
+                        db.Query<int>($"UPDATE ColaArticulos " +
+                        $"SET Cantidad = @Cantidad " +
+                        $",FechaActualizacion = @FechaActualizacion " +
+                        $",Sincronizado = @Sincronizado " +
+                        $"WHERE ID = @ID ", dbparams, commandType: CommandType.Text, transaction: tran).FirstOrDefault();
+                    }
+
+                    tran.Commit();
+                }
+                catch (Exception ex)
+                {
+                    tran.Rollback();
+                    throw ex;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (db.State == ConnectionState.Open)
+                    db.Close();
+            }
         }
     }
 }
